a6c2af7028b32f31fcc2e766b4de83b57ff238e5
moktarul <moktarul12@gmail.com>
2026-06-29 21:31:08 +0530

    change package to com.dromominds.tashbeeh

--- android/app/src/main/java/com/dromominds/tashbeeh/MainActivity.kt
+++ android/app/src/main/java/com/dromominds/tashbeeh/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.moktarul.tashbeeapp
+package com.dromominds.tashbeeh
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/dromominds/tashbeeh/MainApplication.kt
+++ android/app/src/main/java/com/dromominds/tashbeeh/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.moktarul.tashbeeapp
+package com.dromominds.tashbeeh
 
 import android.app.Application
 import android.content.res.Configuration
